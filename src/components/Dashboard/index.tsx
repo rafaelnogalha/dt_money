26dfe0ab1d87afdd5efb5a1952962d04309394f3
rafaelnogalha <rafaelnogalha@gmail.com>
2021-09-05 00:45:11 -0300

    first commit

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionTable";
+import { Container } from "./styles";
+
+export function Dashboard(){
+
+  return(
+    <Container>
+      <Summary />
+      <TransactionsTable />
+    </Container>
+  );
+
+}
